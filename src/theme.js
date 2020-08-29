dd3d7eeb6c52189aa16e51828b882d67d337c8d2
Gabriel G <base@gbr.pw>
2020-08-28 22:30:05 -0500

    First app commit

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import * as styledComponents from "styled-components";
+
+const {
+  default: styled,
+  css,
+  injectGlobal,
+  keyframes,
+  ThemeProvider
+} = styledComponents;
+
+export const theme = {
+  color: "black"
+};
+
+export default styled;
+export { css, injectGlobal, keyframes, ThemeProvider };
